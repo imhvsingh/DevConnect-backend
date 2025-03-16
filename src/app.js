0309c8529a0d71eea0454f2132fff29ffaf82923
Harsh Vardhan Singh <hvsingh5732@gmail.com>
2025-03-16 21:50:52 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,7 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
+//This will only match the GET HTTP method API calls to /user
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Harsh", lastName: "singh" });
+});
+
+app.post("/user", (req, res) => {
+    //Save user data to the database
+    res.send("User data saved successfully!");
+});
+
+app.delete("/user", (req, res) => {
+    //Delete user data from the database
+    res.send("User data deleted successfully!");
+});
+
+//This will match all the HTTP method API calls to /test
+app.use("/test", (req, res) => {
     res.send("namaste from the server!");
 });
 
